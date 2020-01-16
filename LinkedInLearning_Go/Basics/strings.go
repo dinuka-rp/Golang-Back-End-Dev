de89f9ea5680c3103309f1c4c051e93b6fe5526b
Dinuka <drpiyadigama@gmail.com>
2020-01-17 01:50:11 +0530

    More on Strings

--- LinkedInLearning_Go/Basics/strings.go
+++ LinkedInLearning_Go/Basics/strings.go
@@ -16,4 +16,26 @@ func main()  {
 	// Strings in go are immutable
 	// book[0] = 116		// gives an error
 	
+	// Slice (start, end)
+	fmt.Println(book[4:11])
+
+	// Slice (no end)
+	fmt.Println(book[4:])
+
+	// Slice (no start)
+	fmt.Println(book[:4])
+
+	// String concatenation
+	fmt.Println("t"+book[1:])
+
+	// Unicode support
+	fmt.Println("☆")
+	
+	//Multi line strings using back tick ``
+	poem := `
+	The road goes ever on
+	Down from the door where it began
+	...
+	`
+	fmt.Println(poem)
 }
