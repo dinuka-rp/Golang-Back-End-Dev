325dfd9dc3c871215f461db5c966ed640383477f
Dinuka <drpiyadigama@gmail.com>
2020-01-16 13:43:35 +0530

    Decimal calculations with Go

--- LinkedInLearning_Go/Basics/mean.go
+++ LinkedInLearning_Go/Basics/mean.go
@@ -6,8 +6,10 @@ import(
 )
 
 func main(){
-	var x int
-	var y int
+	// var x int
+	// var y int
+	var x float64
+	var y float64
 
 	x=1 
 	y=2
@@ -15,7 +17,9 @@ func main(){
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
-	var mean int
-	mean = (x+y)/2
+	// var mean int
+	var mean float64
+	// mean = (x+y)/2
+	mean = (x+y)/2.0
 	fmt.Printf("Result: %v, type of %T\n", mean, mean)
 }
