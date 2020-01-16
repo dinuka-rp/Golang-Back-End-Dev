5eed5367c466580d31308784a764db6e5124cb71
Dinuka <drpiyadigama@gmail.com>
2020-01-16 13:46:28 +0530

    Auto intialization of variables

--- LinkedInLearning_Go/Basics/mean.go
+++ LinkedInLearning_Go/Basics/mean.go
@@ -8,18 +8,18 @@ import(
 func main(){
 	// var x int
 	// var y int
-	var x float64
-	var y float64
+	// var x float64
+	// var y float64
 
-	x=1 
-	y=2
+	x := 1.0 
+	y := 2.0
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
 	// var mean int
-	var mean float64
+	// var mean float64
 	// mean = (x+y)/2
-	mean = (x+y)/2.0
+	mean := (x+y)/2.0
 	fmt.Printf("Result: %v, type of %T\n", mean, mean)
 }
