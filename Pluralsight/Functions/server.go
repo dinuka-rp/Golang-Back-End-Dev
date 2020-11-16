a2b5fae1f0cdc3ac5d9856b0a2121da6877a7400
dinuka-rp <drpiyadigama@gmail.com>
2020-11-16 14:50:33 +0530

    Create function with multiple return types

--- Pluralsight/Functions/server.go
+++ Pluralsight/Functions/server.go
@@ -1,23 +1,27 @@
 package main
 
 import (
-	"errors"
+	//"errors"
 	"fmt"
 )
 
 func main(){
 	fmt.Println("Hello, playground")
 	port := 3000
-	err := startWebServer(port, 2)
-	fmt.Println(err)
+	_, err := startWebServer(port, 2)
+	fmt.Println( err)
+
+	retPort, err := startWebServer(port, 2)
+	fmt.Println( retPort, err)
 }
 
-func startWebServer(port, numberOfRetries int) error {		// data type followed after multiple commas will be applied to all vars before it
+func startWebServer(port, numberOfRetries int) (int, error) {		// data type followed after multiple commas will be applied to all vars before it
 	fmt.Println("Start server...")
 
 	fmt.Println("Server started on port", port)
 	fmt.Println("Number of retries", numberOfRetries)
 
 	//return nil
-	return errors.New("something went wrong")
+	//return errors.New("something went wrong")
+return port, nil
 }
