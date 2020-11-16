5e45013da4f0bea38ffb9f9b65f6b1f023287520
dinuka-rp <drpiyadigama@gmail.com>
2020-11-16 14:43:31 +0530

    Add a function which returns an error

--- Pluralsight/Functions/server.go
+++ Pluralsight/Functions/server.go
@@ -1,19 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main(){
 	fmt.Println("Hello, playground")
 	port := 3000
-	isStarted := startWebServer(port, 2)
-	fmt.Println(isStarted)
+	err := startWebServer(port, 2)
+	fmt.Println(err)
 }
 
-func startWebServer(port, numberOfRetries int) bool {		// data type followed after multiple commas will be applied to all vars before it
+func startWebServer(port, numberOfRetries int) error {		// data type followed after multiple commas will be applied to all vars before it
 	fmt.Println("Start server...")
 
 	fmt.Println("Server started on port", port)
 	fmt.Println("Number of retries", numberOfRetries)
 
-	return true
+	//return nil
+	return errors.New("something went wrong")
 }
