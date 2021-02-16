cd3b92350f69ef237479fc5c2ea37f4369e5f058
dinuka-rp <drpiyadigama@gmail.com>
2021-02-16 13:46:41 +0530

    create concurrent functions

--- Pluralsight/Concurrency/Bookstore/main.go
+++ Pluralsight/Concurrency/Bookstore/main.go
@@ -12,21 +12,26 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano())) // random number gener
 func main() {
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1 // book IDs 1-10
-		if b, ok := queryCache(id); ok {
-			//	if ok is true
-			fmt.Println("from cache")
-			fmt.Println(b)
-			continue
-		}
-		if b, ok := queryDatabase(id); ok {
-			fmt.Println("from database")
-			fmt.Println(b)
-			continue
-		}
 
-		fmt.Printf("Book not found with id: '%v'", id)
-		time.Sleep(150 * time.Millisecond)
+		go func(id int) {
+			if b, ok := queryCache(id); ok {
+				//	if ok is true
+				fmt.Println("from cache")
+				fmt.Println(b)
+			}
+		}(id)
+
+		go func(id int) {
+			if b, ok := queryDatabase(id); ok {
+				fmt.Println("from database")
+				fmt.Println(b)
+			}
+		}(id)
+
+		//fmt.Printf("Book not found with id: '%v'", id)
+		time.Sleep(150 * time.Millisecond)		// used to warmup the cache
 	}
+	time.Sleep(2 * time.Millisecond)		// to make sure that all the go routines are completed
 }
 
 func queryCache(id int) (Book, bool) {
