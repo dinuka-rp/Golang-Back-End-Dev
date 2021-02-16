4a9e4141268ce9404747b6c33684aeac95e15234
dinuka-rp <drpiyadigama@gmail.com>
2021-02-16 14:44:27 +0530

    create a buffered channel

--- Pluralsight/Concurrency/channels.go
+++ Pluralsight/Concurrency/channels.go
@@ -8,7 +8,23 @@ import (
 func main() {
 	wg := &sync.WaitGroup{}
 	// unbuffered channel
-	ch := make(chan int) // make channel with type of message the channel is going to be working with
+	//ch := make(chan int) // make channel with type of message the channel is going to be working with
+	//
+	//wg.Add(2)
+	//
+	//go func(ch chan int, wg *sync.WaitGroup) {
+	//	fmt.Println(<-ch) // receive a message from a channel
+	//	wg.Done()
+	//}(ch, wg)
+	//
+	//go func(ch chan int, wg *sync.WaitGroup) {
+	//	ch <- 42 //	pass a message into the channel
+	//	wg.Done()
+	//}(ch, wg)
+
+	// buffered channel
+	ch := make(chan int, 1) // make channel with type of message the channel is going to be working with
+	// , internal capacity of the channel
 
 	wg.Add(2)
 
@@ -19,6 +35,7 @@ func main() {
 
 	go func(ch chan int, wg *sync.WaitGroup) {
 		ch <- 42 //	pass a message into the channel
+		ch <- 27
 		wg.Done()
 	}(ch, wg)
 
