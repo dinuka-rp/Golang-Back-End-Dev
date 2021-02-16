1c0be74d015215119d3778f57c314754d8a57573
dinuka-rp <drpiyadigama@gmail.com>
2021-02-16 14:22:27 +0530

    use RWMutexes to allow multiple go routines to read from a resource

--- Pluralsight/Concurrency/Bookstore/main.go
+++ Pluralsight/Concurrency/Bookstore/main.go
@@ -12,12 +12,12 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano())) // random number gener
 
 func main() {
 	wg := &sync.WaitGroup{} // & => pointer to the waitgroup
-	m := &sync.Mutex{}
+	m := &sync.RWMutex{}
 
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1 // book IDs 1-10
 		wg.Add(2)              // how many concurrent tasks that the wait group will wait for
-		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
 			if b, ok := queryCache(id, m); ok {
 				//	if ok is true
 				fmt.Println("from cache")
@@ -26,7 +26,7 @@ func main() {
 			wg.Done()
 		}(id, wg, m)
 
-		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
 			if b, ok := queryDatabase(id, m); ok {
 				fmt.Println("from database")
 				fmt.Println(b)
@@ -42,18 +42,18 @@ func main() {
 	time.Sleep(2 * time.Millisecond) // to make sure that all the go routines are completed
 }
 
-func queryCache(id int, m *sync.Mutex) (Book, bool) {
-	m.Lock()
+func queryCache(id int, m *sync.RWMutex) (Book, bool) {
+	m.RLock()		// multiple readers
 	b, ok := cache[id] // ok= true/ false if value is received/ not
-	m.Unlock()
+	m.RUnlock()
 	return b, ok
 }
 
-func queryDatabase(id int, m *sync.Mutex) (Book, bool) {
+func queryDatabase(id int, m *sync.RWMutex) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range books {
 		if b.ID == id {
-			m.Lock()
+			m.Lock()		// no readers - only one writer
 			cache[id] = b // update cache with newly found book
 			m.Unlock()
 			return b, true
