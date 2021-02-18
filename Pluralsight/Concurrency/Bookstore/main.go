53fd055db33bd023baeb29fa22dcceb954ad6805
dinuka-rp <drpiyadigama@gmail.com>
2021-02-18 23:31:40 +0530

    utlize channels to query cache & db in bookstore

--- Pluralsight/Concurrency/Bookstore/main.go
+++ Pluralsight/Concurrency/Bookstore/main.go
@@ -14,26 +14,45 @@ func main() {
 	wg := &sync.WaitGroup{} // & => pointer to the waitgroup
 	m := &sync.RWMutex{}
 
+	cacheCh := make(chan Book) // channel to hold cache results
+	dbCh := make(chan Book)    // channel to hold db results
+
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1 // book IDs 1-10
 		wg.Add(2)              // how many concurrent tasks that the wait group will wait for
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
 			if b, ok := queryCache(id, m); ok {
 				//	if ok is true
-				fmt.Println("from cache")
-				fmt.Println(b)
+				//fmt.Println("from cache")
+				//fmt.Println(b)
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, m)
+		}(id, wg, m, cacheCh)
 
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
 			if b, ok := queryDatabase(id, m); ok {
+				//fmt.Println("from database")
+				//fmt.Println(b)
+				ch <- b
+			}
+			wg.Done()
+
+		}(id, wg, m, dbCh)
+
+		// 10 receiving operations (go routines) are needed, because 10 generators are created
+		go func(cacheCh, dbCh <-chan Book) { // receive only channels
+			select {
+			case b := <-cacheCh:
+				fmt.Println("from cache")
+				fmt.Println(b)
+				// never drain the message coming from the db
+				<-dbCh // receive operation: to make sure that the 2nd go routine isn't blocked
+			case b := <-dbCh:
 				fmt.Println("from database")
 				fmt.Println(b)
 			}
-			wg.Done()
-
-		}(id, wg, m)
+		}(cacheCh, dbCh)
 
 		//fmt.Printf("Book not found with id: '%v'", id)
 		time.Sleep(150 * time.Millisecond) // used to warmup the cache
@@ -43,7 +62,7 @@ func main() {
 }
 
 func queryCache(id int, m *sync.RWMutex) (Book, bool) {
-	m.RLock()		// multiple readers
+	m.RLock()          // multiple readers
 	b, ok := cache[id] // ok= true/ false if value is received/ not
 	m.RUnlock()
 	return b, ok
@@ -53,7 +72,7 @@ func queryDatabase(id int, m *sync.RWMutex) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range books {
 		if b.ID == id {
-			m.Lock()		// no readers - only one writer
+			m.Lock()      // no readers - only one writer
 			cache[id] = b // update cache with newly found book
 			m.Unlock()
 			return b, true
